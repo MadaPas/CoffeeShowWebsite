31f0d1a59d198046b84382141fdcd1f85cfdd7fc
madapas <mada0193@stud.kea.dk>
2020-07-20 15:08:43 +0200

    coffees route - all coffees & coffees by category id

--- routes/coffees.js
+++ routes/coffees.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
 
+const Coffee = require('../models/Coffee.js');
 
+router.get('/coffees', async (req, res) => {
+    const coffees = await Coffee.query().select();
+    return res.send({ response: coffees });
+})
+
+router.get('/coffees/category/:categoryId', async(req, res) => {
+    const coffeesInCategory = await Coffee.query().select().where({ 'categoryId': req.params.categoryId });
+    if (coffeesInCategory.length > 0) {
+        return res.send({ response: coffeesInCategory });
+    } else {
+        return res.status(400).send({ response: "No coffee in the requested category has found." });
+    }
+
+})
 
 module.exports = router;
