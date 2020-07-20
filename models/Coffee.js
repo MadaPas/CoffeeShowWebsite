683f15497a44846bc7df4fc6732a090fc889a8ef
madapas <mada0193@stud.kea.dk>
2020-07-20 14:47:19 +0200

    Coffee model - relation mappings done

--- models/Coffee.js
+++ models/Coffee.js
@@ -3,7 +3,21 @@ const { Model } = require('objection');
 const Category = require('./Category.js');
 
 class Coffee extends Model {
-// yada yada
+
+    static tableName = 'coffees';
+
+    static relationMappings = {
+        category: {
+            relation: Model.BelongsToOneRelation,
+            modelClass: Category,
+            join: {
+                from: 'coffees.categoryId',
+                to: 'categories.id'
+            }
+        }
+
+    }
+
 }
 
 module.exports = Coffee;
