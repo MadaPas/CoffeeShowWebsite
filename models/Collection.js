589f0bb80c44451c7d0368d8be1034aed92b0ef2
madapas <mada0193@stud.kea.dk>
2020-07-20 14:49:46 +0200

    Collection model - relation mappings done

--- models/Collection.js
+++ models/Collection.js
@@ -4,6 +4,20 @@ const Brand = require('./Brand.js');
 
 class Collection extends Model {
 
+    static tableName = 'collections';
+
+    static relationMappings = {
+        brand: {
+            relation: Model.HasManyRelation,
+            modelClass: Brand,
+            join: {
+                from: 'collections.brandId',
+                to: 'brands.id'
+            }
+        }
+
+    }
+
 }
 
 module.exports = Collection;
