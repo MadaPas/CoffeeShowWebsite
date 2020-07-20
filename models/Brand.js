615a67c2147ed980c0db3134611a2d925e3f5fdc
madapas <mada0193@stud.kea.dk>
2020-07-20 14:44:27 +0200

    Brand model - relation mappings done

--- models/Brand.js
+++ models/Brand.js
@@ -3,7 +3,21 @@ const { Model } = require('objection');
 const Collection = require('./Collection.js');
 
 class Brand extends Model {
- // yada yada
+
+    static tableName = 'brands';
+
+    static relationMappings = {
+        collection: {
+            relation: Model.HasManyRelation,
+            modelClass: Collection,
+            join: {
+                from: 'brands.id',
+                to: 'collections.brandId'
+            }
+
+        }
+    }
+
 }
 
 module.exports = Brand;
