891450a1b8a85458fc96b392d73dda44a2cd4181
madapas <mada0193@stud.kea.dk>
2020-08-04 21:03:22 +0200

    user seed updated

--- seeds/005._users.js
+++ seeds/005._users.js
@@ -1,15 +1,15 @@
 exports.seed = function (knex) {
 
   return knex('users').insert([{
-      username: "admin",
-      email: "admin@admin-area.com",
-      password: "$2b$12$yVafDZv8E5SOQ05S.f7W4ejoLwIBZbHMStCLw9ApyTHFguprz8rDK"
-    }, //admin_pass
+      username: "madalina",
+      email: "madalina@email.com",
+      password: "$2b$12$HX62.pngv.KlAIIGQuk7H.4u3NuetCuS2OjRKBuapd8zm7jJO13bS"
+    }, //madalina
     {
       username: "user",
       email: "user@user-area.com",
-      password: "$2b$12$sEMO6SL5tcA98DNSCAvaI.3zBa1Mf43k9cpCFYbnnKKbneYeuvYHC"
-    } //user_pass
+      password: "$2b$12$YR3yXYNTxJs9ZeDatAHt2eaR9UQQYkr0itmSuxAUwW30oSf8lfP0a"
+    } //password
   ]);
 
 };
