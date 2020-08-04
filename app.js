989dca6f33ab3f91e062ece0b07687243107b8ae
madapas <mada0193@stud.kea.dk>
2020-08-04 08:05:28 +0200

    new routes: session & functionality setup

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
-const session = require("express-session");
+const session = require('express-session');
+const rateLimit = require('express-rate-limit');
 const fs = require('fs');
 
-
-// Enable express to parse json
+/*
+    Enable express to parse json
+*/
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 
-/* session */
-const config = require("./config/config.json");
+app.use(express.static('public'));
+
+/* 
+    Session 
+*/
+const config = require('./config/config.json');
 
 // used as middleware, sits between the request and the response
 app.use(session({
@@ -19,9 +27,9 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-/* rate limiter */
-const rateLimit = require('express-rate-limit');
-
+/* 
+    rate limiter 
+*/
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100 // limit each IP to 100 requests per windowMs
@@ -39,9 +47,13 @@ app.use('/register', authLimiter);
 app.use('/login', authLimiter);
 
 
-/* Setup Knex with Objection */
+/* 
+    Setup Knex with Objection 
+*/
 
-const { Model } = require('objection');
+const {
+    Model
+} = require('objection');
 const Knex = require('knex');
 // knexFile contains our meta information about our connection
 const knexFile = require('./knexfile.js');
@@ -54,15 +66,18 @@ const knex = Knex(knexFile.development); // This is how you can have different e
 // Connect the knex to our objection model, so that objection knows to use knex
 Model.knex(knex);
 
+/*
+    hashing passwords
+/*
+
 // const saltRounds = 12;
-// bcrypt.hash("admin_pass", saltRounds, function(err, hash) {
-//     console.log("admin:", hash);
+// bcrypt.hash('admin_pass', saltRounds, function(err, hash) {
+//     console.log('admin:', hash);
 // });
-// bcrypt.hash("user_pass", saltRounds, function(err, hash) {
-//     console.log("user:", hash);
+// bcrypt.hash('user_pass', saltRounds, function(err, hash) {
+//     console.log('user:', hash);
 // });
 
-app.use(express.static('public'));
 
 /*
     Routes
@@ -77,49 +92,12 @@ const countryRoute = require('./routes/countries.js');
 app.use(countryRoute);
 const specialtyRoute = require('./routes/specialties.js');
 app.use(specialtyRoute);
-// const authRoute = require('./routes/auth.js');
-// app.use(authRoute);
-
-const header = fs.readFileSync("./public/main/header.html", "utf8");
-const footer = fs.readFileSync("./public/main/footer.html", "utf8");
-const navbar = fs.readFileSync("./public/main/navbar.html", "utf8");
-const userNavbar = fs.readFileSync("./public/main/navbar-user.html", "utf8");
-
-
-const mainP = fs.readFileSync("./public/main/main-page.html", "utf8");
-const homeP = fs.readFileSync("./public/main/home/home-page.html", "utf8");
-
-
-const loginPage = (req, res, next) => {
-    if (!req.session.user) {
-        res.redirect('/login');
-    } else {
-        next();
-    }
-}
-
-const homePage = (req, res, next) => {
-    if (req.session.user) {
-        res.redirect('/home-page');
-    } else {
-        next();
-    }
-}
-
-app.get('/', homePage, (req, res) => {
-    console.log("session: ", req.sessionID);
-    console.log("user: ", req.session.user);
-
-    return res.send(header + navbar + mainP + footer);
-})
-
-app.get('/home', loginPage, (req, res) => {
-    console.log("session: ", req.sessionID);
-    console.log("user: ", req.session.user);
-
-    return res.send(header + userNavbar + mainP + footer)
-
-})
+const authRoute = require('./routes/auth.js');
+app.use(authRoute);
+const functionalityRoute = require('./routes/functionality.js');
+app.use(functionalityRoute);
+const sessionRoute = require('./routes/session.js');
+app.use(sessionRoute);
 
 
 /* 
@@ -132,5 +110,5 @@ app.listen(PORT, (error) => {
     if (error) {
         console.log(error);
     }
-    console.log("Server is running on port", PORT);
+    console.log('Server is running on port', PORT);
 });
