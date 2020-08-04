adf55ff200db7f5c2b9e4bce7fbb732c779a8d90
madapas <mada0193@stud.kea.dk>
2020-08-04 11:47:18 +0200

    logout small change

--- routes/coffees.js
+++ routes/coffees.js
@@ -1,6 +1,32 @@
 const router = require('express').Router();
+const fs = require('fs');
 
 const Coffee = require('../models/Coffee.js');
+
+/* 
+    Add html files 
+*/
+
+const header = fs.readFileSync('./public/fragments/header.html', 'utf8');
+const footer = fs.readFileSync('./public/fragments/footer.html', 'utf8');
+const homeNav = fs.readFileSync('./public/fragments/homeNav.html', 'utf8');
+const searchCoffees = fs.readFileSync('./public/fragments/search/search-coffees.html', 'utf8');
+
+/*
+    if there's no user logged in -> redirect to the login page
+*/
+const login = (req, res, next) => {
+    if (!req.session.user) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
+router.get('/search-coffees', login, (req, res) => {
+    return res.send(header + homeNav + searchCoffees + footer);
+});
+
 
 // all coffees
 router.get('/coffees', async (req, res) => {
@@ -14,7 +40,7 @@ router.get('/coffees/name/:coffee_name', async(req, res) => {
     if (foundCoffee.length > 0) {
         return res.send({ response: foundCoffee });
     } else {
-        return res.status(400).send({ response: "There were no coffees of the name found." });
+        return res.status(400).send({ response: 'There were no coffees of the name found.' });
     }
 })
 
@@ -24,7 +50,7 @@ router.get('/coffees/size/:coffee_size', async(req, res) => {
     if (coffeeSize.length > 0) {
         return res.send({ response: coffeeSize });
     } else {
-        return res.status(400).send({ response: "There were no coffees of the requested size found." });
+        return res.status(400).send({ response: 'There were no coffees of the requested size found.' });
     }
 })
 
@@ -34,7 +60,7 @@ router.get('/coffees/category/:categoryId', async(req, res) => {
     if (coffeesCategory.length > 0) {
         return res.send({ response: coffeesCategory });
     } else {
-        return res.status(400).send({ response: "There were no coffees in the requested category found." });
+        return res.status(400).send({ response: 'There were no coffees in the requested category found.' });
     }
 })
 
